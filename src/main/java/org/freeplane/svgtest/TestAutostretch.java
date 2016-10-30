286b804480ac41c2352baa53814aba0dd1a94e57
Felix Natter <fnatter@gmx.net>
2016-10-30 18:26:10 +0100

    finish test case

--- src/main/java/org/freeplane/svgtest/TestAutostretch.java
+++ src/main/java/org/freeplane/svgtest/TestAutostretch.java
@@ -1,22 +1,52 @@
 package org.freeplane.svgtest;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.net.URI;
+import java.net.URL;
+
 import javax.swing.*;
+
+import com.kitfox.svg.SVGUniverse;
+import com.kitfox.svg.app.beans.SVGIcon;
 
 public class TestAutostretch {
 
+	private static SVGUniverse svgUniverse;
+	private final static String TEST_SVG = "button_ok.svg";
+
     private static void createAndShowGUI() {
-        //Create and set up the window.
         JFrame frame = new JFrame("svgSalamander Autostretch test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
+        final URL resource = ClassLoader.getSystemResource(TEST_SVG);
+        final SVGIcon testIcon = createSVGIcon(resource, 256, 256);
+        final JLabel label = new JLabel(testIcon);
+        label.setBorder(BorderFactory.createLineBorder(Color.black));
         frame.getContentPane().add(label);
 
-        //Display the window.
         frame.pack();
         frame.setVisible(true);
     }
+
+	private static SVGIcon createSVGIcon(final URL url, final int widthPixels, final int heightPixels) {
+		if (svgUniverse == null)
+			svgUniverse = new SVGUniverse();
+
+		final SVGIcon icon = new SVGIcon();
+		URI svgUri;
+		try {
+			svgUri = svgUniverse.loadSVG(url.openStream(), url.getPath());
+			icon.setSvgUniverse(svgUniverse);
+			icon.setSvgURI(svgUri);
+			icon.setPreferredSize(new Dimension(widthPixels, heightPixels));
+			icon.setAutosize(SVGIcon.AUTOSIZE_VERT);
+			icon.setAntiAlias(true);
+			return icon;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public static void main(String[] args) {
         //Schedule a job for the event-dispatching thread:
@@ -27,5 +57,5 @@ public class TestAutostretch {
             }
         });
     }
-
+ 
 }
