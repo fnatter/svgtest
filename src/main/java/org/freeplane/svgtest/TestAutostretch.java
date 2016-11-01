104bdb8588e5d2e889c99a5d380072d7823d245f
Felix Natter <fnatter@gmx.net>
2016-11-01 16:07:13 +0100

    add examples for AUTOSIZE_{STRETCH,HORIZ,VERT}

--- src/main/java/org/freeplane/svgtest/TestAutostretch.java
+++ src/main/java/org/freeplane/svgtest/TestAutostretch.java
@@ -2,34 +2,64 @@ package org.freeplane.svgtest;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.net.URI;
 import java.net.URL;
 
-import javax.swing.*;
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import com.kitfox.svg.SVGUniverse;
 import com.kitfox.svg.app.beans.SVGIcon;
 
 public class TestAutostretch {
 
+	private static final int PREFERREDSIZE_HEIGHT = 128;
+	private static final int PREFERREDSIZE_WIDTH = 128;
 	private static SVGUniverse svgUniverse;
 	private final static String TEST_SVG = "button_ok.svg";
 
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("svgSalamander Autostretch test");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
 
         final URL resource = ClassLoader.getSystemResource(TEST_SVG);
-        final SVGIcon testIcon = createSVGIcon(resource, 256, 256);
-        final JLabel label = new JLabel(testIcon);
-        label.setBorder(BorderFactory.createLineBorder(Color.black));
-        frame.getContentPane().add(label);
+        
+        final JPanel exampleImagesPanel = new JPanel();
+        exampleImagesPanel.setLayout(new FlowLayout());
+        
+        exampleImagesPanel.add(new JLabel(String.format("%dx%d, AUTOSIZE_STRETCH", PREFERREDSIZE_WIDTH, PREFERREDSIZE_HEIGHT)));
+        final SVGIcon testIcon1 = createSVGIcon(resource, PREFERREDSIZE_WIDTH, PREFERREDSIZE_HEIGHT,
+        		SVGIcon.AUTOSIZE_STRETCH);
+        final JLabel label1 = new JLabel(testIcon1);
+        label1.setBorder(BorderFactory.createLineBorder(Color.black));
+        exampleImagesPanel.add(label1);
+        
+        exampleImagesPanel.add(new JLabel(String.format("%dx%d, AUTOSIZE_HORIZ", PREFERREDSIZE_WIDTH, PREFERREDSIZE_HEIGHT)));
+        final SVGIcon testIcon2 = createSVGIcon(resource, PREFERREDSIZE_WIDTH, PREFERREDSIZE_HEIGHT,
+        		SVGIcon.AUTOSIZE_HORIZ);
+        final JLabel label2 = new JLabel(testIcon2);
+        label2.setBorder(BorderFactory.createLineBorder(Color.black));
+        exampleImagesPanel.add(label2);
+
+        exampleImagesPanel.add(new JLabel(String.format("%dx%d, AUTOSIZE_VERT", PREFERREDSIZE_WIDTH, PREFERREDSIZE_HEIGHT)));
+        final SVGIcon testIcon3 = createSVGIcon(resource, PREFERREDSIZE_WIDTH, PREFERREDSIZE_HEIGHT,
+        		SVGIcon.AUTOSIZE_VERT);
+        final JLabel label3 = new JLabel(testIcon3);
+        label3.setBorder(BorderFactory.createLineBorder(Color.black));
+        exampleImagesPanel.add(label3);
+
+        frame.getContentPane().add(exampleImagesPanel);
 
         frame.pack();
         frame.setVisible(true);
     }
 
-	private static SVGIcon createSVGIcon(final URL url, final int widthPixels, final int heightPixels) {
+	private static SVGIcon createSVGIcon(final URL url, final int widthPixels, final int heightPixels,
+			final int autosize) {
 		if (svgUniverse == null)
 			svgUniverse = new SVGUniverse();
 
@@ -40,7 +70,7 @@ public class TestAutostretch {
 			icon.setSvgUniverse(svgUniverse);
 			icon.setSvgURI(svgUri);
 			icon.setPreferredSize(new Dimension(widthPixels, heightPixels));
-			icon.setAutosize(SVGIcon.AUTOSIZE_VERT);
+			icon.setAutosize(autosize);
 			icon.setAntiAlias(true);
 			return icon;
 		} catch (Exception e) {
